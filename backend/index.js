420d765bbe7915c0280fc535e4a2b288f765c4f7
Markus Wieland <wie.markus@web.de>
2021-11-17 22:28:29 +0100

    fixed undefined env variable bug

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require('dotenv').config();
 const app = express();
 const port = 1108;
 const axios = require("axios");
@@ -6,7 +7,7 @@ const axios = require("axios");
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 app.get("/weather", (request, response) => {
-    response.set("Access-Control-Allow-Origin", "*")
+    response.set("Access-Control-Allow-Origin", "*");
     axios("https://api.openweathermap.org/data/2.5/weather?q=Dresden,01277&units=metric&appid=" + WEATHER_API_KEY)
         .then(result => response.send(result.data));
 });
